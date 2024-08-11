777366b28357db97dabd627527a23ead7d6fa704
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-11 19:05:52 +0800

    final

--- private_src/IndependentWatchDog.h
+++ private_src/IndependentWatchDog.h
@@ -1,51 +1,51 @@
 #pragma once
-#include <IndependentWatchDogConfig.h>
 #include <base/HandleWrapper.h>
 #include <bsp-interface/IIndependentWatchDog.h>
 #include <chrono>
+#include <IndependentWatchDogConfig.h>
 
 namespace hal
 {
-	/// @brief 独立看门狗。
-	/// @note 所谓独立看门狗就是具有自己的内部时钟源，不依赖单片机的系统时钟。
-	/// 在系统时钟失效时仍然能工作。
-	class IndependentWatchDog
-		: public bsp::IIndependentWatchDog,
-		  public base::HandleWrapper<IWDG_HandleTypeDef>
-	{
-	private:
-		IWDG_HandleTypeDef _handle;
-		IndependentWatchDogConfig _config;
+    /// @brief 独立看门狗。
+    /// @note 所谓独立看门狗就是具有自己的内部时钟源，不依赖单片机的系统时钟。
+    /// 在系统时钟失效时仍然能工作。
+    class IndependentWatchDog final :
+        public bsp::IIndependentWatchDog,
+        public base::HandleWrapper<IWDG_HandleTypeDef>
+    {
+    private:
+        IWDG_HandleTypeDef _handle;
+        IndependentWatchDogConfig _config;
 
-		/// @brief 内部时钟信号的频率。还要经过预分频才会输入到计数器。
-		/// @return
-		uint32_t InnerClockSourceFreq_Hz() const
-		{
-			// 独立看门狗具有 40 kHz 的内部时钟。
-			return 40 * 1000;
-		}
+        /// @brief 内部时钟信号的频率。还要经过预分频才会输入到计数器。
+        /// @return
+        uint32_t InnerClockSourceFreq_Hz() const
+        {
+            // 独立看门狗具有 40 kHz 的内部时钟。
+            return 40 * 1000;
+        }
 
-		void Initialize();
+        void Initialize();
 
-	public:
-		static IndependentWatchDog &Instance()
-		{
-			static IndependentWatchDog o;
-			return o;
-		}
+    public:
+        static IndependentWatchDog &Instance()
+        {
+            static IndependentWatchDog o;
+            return o;
+        }
 
-		IWDG_HandleTypeDef &Handle() override
-		{
-			return _handle;
-		}
+        IWDG_HandleTypeDef &Handle() override
+        {
+            return _handle;
+        }
 
-		IWDG_TypeDef *HardwareInstance()
-		{
-			return IWDG;
-		}
+        IWDG_TypeDef *HardwareInstance()
+        {
+            return IWDG;
+        }
 
-		std::chrono::milliseconds WatchDogTimeoutDuration() const override;
-		void SetWatchDogTimeoutDuration(std::chrono::milliseconds value) override;
-		void Feed() override;
-	};
-}
+        std::chrono::milliseconds WatchDogTimeoutDuration() const override;
+        void SetWatchDogTimeoutDuration(std::chrono::milliseconds value) override;
+        void Feed() override;
+    };
+} // namespace hal
