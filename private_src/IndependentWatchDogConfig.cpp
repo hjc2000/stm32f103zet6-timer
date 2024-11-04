0df0947fd684478b75c9af97a8611f89b423aafe
hjc2000 <hjc.666@foxmail.com>
2024-11-04 17:07:23 +0800

    IndependentWatchDogConfig_PrescalerOption

--- private_src/IndependentWatchDogConfig.cpp
+++ private_src/IndependentWatchDogConfig.cpp
@@ -14,12 +14,12 @@ bsp::IndependentWatchDogConfig &bsp::IndependentWatchDogConfig::operator=(IWDG_I
 
 #pragma region 分频系数
 
-bsp::IndependentWatchDogConfig::PrescalerOption bsp::IndependentWatchDogConfig::Prescaler() const
+bsp::IndependentWatchDogConfig_PrescalerOption bsp::IndependentWatchDogConfig::Prescaler() const
 {
-    return static_cast<PrescalerOption>(_config_handle.Prescaler);
+    return static_cast<IndependentWatchDogConfig_PrescalerOption>(_config_handle.Prescaler);
 }
 
-void bsp::IndependentWatchDogConfig::SetPrescaler(PrescalerOption value)
+void bsp::IndependentWatchDogConfig::SetPrescaler(IndependentWatchDogConfig_PrescalerOption value)
 {
     _config_handle.Prescaler = static_cast<uint32_t>(value);
 }
@@ -28,31 +28,31 @@ uint8_t bsp::IndependentWatchDogConfig::GetPrescalerByPow() const
 {
     switch (Prescaler())
     {
-    case PrescalerOption::Div4:
+    case IndependentWatchDogConfig_PrescalerOption::Div4:
         {
             return 2;
         }
-    case PrescalerOption::Div8:
+    case IndependentWatchDogConfig_PrescalerOption::Div8:
         {
             return 3;
         }
-    case PrescalerOption::Div16:
+    case IndependentWatchDogConfig_PrescalerOption::Div16:
         {
             return 4;
         }
-    case PrescalerOption::Div32:
+    case IndependentWatchDogConfig_PrescalerOption::Div32:
         {
             return 5;
         }
-    case PrescalerOption::Div64:
+    case IndependentWatchDogConfig_PrescalerOption::Div64:
         {
             return 6;
         }
-    case PrescalerOption::Div128:
+    case IndependentWatchDogConfig_PrescalerOption::Div128:
         {
             return 7;
         }
-    case PrescalerOption::Div256:
+    case IndependentWatchDogConfig_PrescalerOption::Div256:
         {
             return 8;
         }
@@ -65,37 +65,37 @@ uint8_t bsp::IndependentWatchDogConfig::GetPrescalerByPow() const
 
 void bsp::IndependentWatchDogConfig::SetPrescalerByPow(uint8_t pow)
 {
-    auto power_to_prescaler = [](uint8_t pow) -> IndependentWatchDogConfig::PrescalerOption
+    auto power_to_prescaler = [](uint8_t pow) -> IndependentWatchDogConfig_PrescalerOption
     {
         switch (pow)
         {
         case 2:
             {
-                return PrescalerOption::Div4;
+                return IndependentWatchDogConfig_PrescalerOption::Div4;
             }
         case 3:
             {
-                return PrescalerOption::Div8;
+                return IndependentWatchDogConfig_PrescalerOption::Div8;
             }
         case 4:
             {
-                return PrescalerOption::Div16;
+                return IndependentWatchDogConfig_PrescalerOption::Div16;
             }
         case 5:
             {
-                return PrescalerOption::Div32;
+                return IndependentWatchDogConfig_PrescalerOption::Div32;
             }
         case 6:
             {
-                return PrescalerOption::Div64;
+                return IndependentWatchDogConfig_PrescalerOption::Div64;
             }
         case 7:
             {
-                return PrescalerOption::Div128;
+                return IndependentWatchDogConfig_PrescalerOption::Div128;
             }
         case 8:
             {
-                return PrescalerOption::Div256;
+                return IndependentWatchDogConfig_PrescalerOption::Div256;
             }
         default:
             {
@@ -111,31 +111,31 @@ uint32_t bsp::IndependentWatchDogConfig::GetPrescalerByUint32() const
 {
     switch (Prescaler())
     {
-    case PrescalerOption::Div4:
+    case IndependentWatchDogConfig_PrescalerOption::Div4:
         {
             return 4;
         }
-    case PrescalerOption::Div8:
+    case IndependentWatchDogConfig_PrescalerOption::Div8:
         {
             return 8;
         }
-    case PrescalerOption::Div16:
+    case IndependentWatchDogConfig_PrescalerOption::Div16:
         {
             return 16;
         }
-    case PrescalerOption::Div32:
+    case IndependentWatchDogConfig_PrescalerOption::Div32:
         {
             return 32;
         }
-    case PrescalerOption::Div64:
+    case IndependentWatchDogConfig_PrescalerOption::Div64:
         {
             return 64;
         }
-    case PrescalerOption::Div128:
+    case IndependentWatchDogConfig_PrescalerOption::Div128:
         {
             return 128;
         }
-    case PrescalerOption::Div256:
+    case IndependentWatchDogConfig_PrescalerOption::Div256:
         {
             return 256;
         }
@@ -152,37 +152,37 @@ void bsp::IndependentWatchDogConfig::SetPrescalerByUint32(uint32_t value)
     {
     case 4:
         {
-            SetPrescaler(PrescalerOption::Div4);
+            SetPrescaler(IndependentWatchDogConfig_PrescalerOption::Div4);
             break;
         }
     case 8:
         {
-            SetPrescaler(PrescalerOption::Div8);
+            SetPrescaler(IndependentWatchDogConfig_PrescalerOption::Div8);
             break;
         }
     case 16:
         {
-            SetPrescaler(PrescalerOption::Div16);
+            SetPrescaler(IndependentWatchDogConfig_PrescalerOption::Div16);
             break;
         }
     case 32:
         {
-            SetPrescaler(PrescalerOption::Div32);
+            SetPrescaler(IndependentWatchDogConfig_PrescalerOption::Div32);
             break;
         }
     case 64:
         {
-            SetPrescaler(PrescalerOption::Div64);
+            SetPrescaler(IndependentWatchDogConfig_PrescalerOption::Div64);
             break;
         }
     case 128:
         {
-            SetPrescaler(PrescalerOption::Div128);
+            SetPrescaler(IndependentWatchDogConfig_PrescalerOption::Div128);
             break;
         }
     case 256:
         {
-            SetPrescaler(PrescalerOption::Div256);
+            SetPrescaler(IndependentWatchDogConfig_PrescalerOption::Div256);
             break;
         }
     default:
